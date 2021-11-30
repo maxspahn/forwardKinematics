9dab98b36fb56447890883e36e07a3a32faf17f2
Max <m.spahn@tudelft.nl>
2021-11-30 18:34:53 +0100

    Makes urdf2casadi an optional dependency

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "casadi", "matplotlib", "urdf2casadi",],
+    install_requires=["numpy", "casadi", "matplotlib",],
+    extras_require={"urdf": ['urdf2casadi']}, 
     python_requires=">=3.6",
 )

a8b011f45eacbbbf8e4c84f1ee671831ee555141
Max <m.spahn@tudelft.nl>
2021-12-17 17:51:19 +0100

    Bug-fix for boxerFk.

--- src/forwardKinematics/urdfFks/boxerFk.py
+++ src/forwardKinematics/urdfFks/boxerFk.py
@@ -7,15 +7,15 @@ class BoxerFk(URDFForwardKinematics):
 
     def __init__(self, n):
         fileName = 'boxer.urdf'
-        relevantLinks = ['base_link', 'ee_link', 'ee_link', 'ee_link']
+        relevantLinks = ['world', 'ee_link', 'ee_link', 'ee_link']
         super().__init__(fileName, relevantLinks, 'world', 3)
 
 
 if __name__ == "__main__":
     q_ca = ca.SX.sym("q_ca", 3)
     bfk = BoxerFk(3)
-    fkCasadi = bfk.fk(q_ca, 3, positionOnly=True)
+    fkCasadi = bfk.fk(q_ca, 0, positionOnly=True)
     print(fkCasadi)
-    q_np = np.array([0.0, 0.0, np.pi/2.0])
+    q_np = np.array([0.0, 1.0, 1 * np.pi/2.0])
     for i in range(bfk.n()):
         print(bfk.fk(q_np, i, positionOnly=True))

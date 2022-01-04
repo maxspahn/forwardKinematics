4cce66082b35fc7aa185c6e6e0e4f63c57d2dc8c
Max <m.spahn@tudelft.nl>
2022-01-04 14:54:32 +0100

    Adds base end-effector for albert. Alligning with boxer.

--- forwardkinematics/urdfFks/albertFk.py
+++ forwardkinematics/urdfFks/albertFk.py
@@ -7,7 +7,7 @@ class AlbertFk(URDFForwardKinematics):
 
     def __init__(self, n):
         fileName = 'albert.urdf'
-        relevantLinks = ['world'] + ['mmrobot_link' + str(i) for i in [0, 0, 0, 3, 4, 5, 6, 7, 8, 9]]
+        relevantLinks = ['world', 'base_tip_link', 'top_mount_bottom'] + ['mmrobot_link' + str(i) for i in [0, 3, 4, 5, 6, 7, 8, 9]]
         super().__init__(fileName, relevantLinks, 'world', len(relevantLinks) - 1)
 
 

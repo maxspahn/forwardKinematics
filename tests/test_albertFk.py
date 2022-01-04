f7af84d151750844296841fdbd3cb3fb8f62aa0e
Max <m.spahn@tudelft.nl>
2022-01-04 16:37:30 +0100

    Adds comparison test panda and albert.

--- tests/test_albertFk.py
+++ tests/test_albertFk.py
@@ -1,6 +1,7 @@
 import casadi as ca
 import numpy as np
 from forwardkinematics.urdfFks.albertFk import AlbertFk
+from forwardkinematics.urdfFks.pandaFk import PandaFk
 import pytest
 
 @pytest.fixture
@@ -40,3 +41,31 @@ def test_fkNonZeros(fk):
     fkNumpy = fk.fk(q_np, 2, positionOnly=True)
     assert isinstance(fkNumpy, np.ndarray)
     assert fkNumpy[2] == pytest.approx(0.2895, abs=1e-4)
+
+def test_arm(fk):
+    q_np = np.array([0, 0, 0, 0.0000, 1.0323, 0.0000, 0.8247, 0.0000, 0.2076, 0.0000])
+    fkBase = fk.fk(q_np, 3, positionOnly=True)
+    fkNumpy = fk.fk(q_np, 3, positionOnly=True)
+    fkPanda = fkNumpy - fkBase
+    x_ee = np.array([0.0, 0.0, 0.0])
+    assert fkPanda[0] == pytest.approx(x_ee[0], abs=1e-4)
+    assert fkPanda[1] == pytest.approx(x_ee[1], abs=1e-4)
+    assert fkPanda[2] == pytest.approx(x_ee[2], abs=1e-4)
+    fkNumpy = fk.fk(q_np, 4, positionOnly=True)
+    fkPanda = fkNumpy - fkBase
+    x_ee = np.array([0.2713, 0.0, 0.4950])
+    assert fkPanda[0] == pytest.approx(x_ee[0], abs=1e-4)
+    assert fkPanda[1] == pytest.approx(x_ee[1], abs=1e-4)
+    assert fkPanda[2] == pytest.approx(x_ee[2], abs=1e-4)
+
+def test_comparePanda(fk):
+    fkPanda = PandaFk(7)
+    q_np = np.array([0, 0, 0, 0.0000, 1.0323, 0.0000, 0.8247, 0.0000, 0.2076, 0.0000])
+    fk_base = fk.fk(q_np, 3, positionOnly=True)
+    for i in range(7):
+        print(i)
+        fk_panda = fkPanda.fk(q_np[3:], i, positionOnly=True)
+        fk_albert = fk.fk(q_np, i+3, positionOnly=True)
+        fk_numpy = fk_albert - fk_base
+        for j in range(3):
+            assert fk_panda[j] == pytest.approx(fk_numpy[j], abs=1e-4)
--- tests/test_pandaFk.py
+++ tests/test_pandaFk.py
@@ -1,13 +1,35 @@
 import casadi as ca
 import numpy as np
+import pytest 
+
 from forwardkinematics.urdfFks.pandaFk import PandaFk
 
+@pytest.fixture
+def fk():
+    return PandaFk(7)
 
-def test_pandaFk():
+def test_pandaFk(fk):
     q_ca = ca.SX.sym("q", 7)
-    fkPlanar = PandaFk(7)
     q_np = np.random.random(7)
-    fkCasadi = fkPlanar.fk(q_ca, 7, positionOnly=False)
-    fkNumpy = fkPlanar.fk(q_np, 7, positionOnly=False)
+    fkCasadi = fk.fk(q_ca, 7, positionOnly=False)
+    fkNumpy = fk.fk(q_np, 7, positionOnly=False)
     assert isinstance(fkCasadi, ca.SX)
     assert isinstance(fkNumpy, np.ndarray)
+
+def test_simpleFk(fk):
+    q_np = np.array([0.0000, 1.0323, 0.0000, 0.8247, 0.0000, 0.2076, 0.0000])
+    fkNumpy = fk.fk(q_np, 0, positionOnly=True)
+    x_ee = np.array([0.0, 0.0, 0.0])
+    assert fkNumpy[0] == pytest.approx(x_ee[0], abs=1e-4)
+    assert fkNumpy[1] == pytest.approx(x_ee[1], abs=1e-4)
+    assert fkNumpy[2] == pytest.approx(x_ee[2], abs=1e-4)
+    fkNumpy = fk.fk(q_np, 1, positionOnly=True)
+    x_ee = np.array([0.2713, 0.0, 0.4950])
+    assert fkNumpy[0] == pytest.approx(x_ee[0], abs=1e-4)
+    assert fkNumpy[1] == pytest.approx(x_ee[1], abs=1e-4)
+    assert fkNumpy[2] == pytest.approx(x_ee[2], abs=1e-4)
+    fkNumpy = fk.fk(q_np, 7, positionOnly=True)
+    x_ee = np.array([0.4, 0.0, 0.69])
+    assert fkNumpy[0] == pytest.approx(x_ee[0], abs=1e-4)
+    assert fkNumpy[1] == pytest.approx(x_ee[1], abs=1e-4)
+    assert fkNumpy[2] == pytest.approx(x_ee[2], abs=1e-4)

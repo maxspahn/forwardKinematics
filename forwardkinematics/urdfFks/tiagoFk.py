dd5120770d59fd30420a4155828d627115dfc300
Max Spahn <m.spahn@tudelft.nl>
2022-03-14 13:06:44 +0100

    Adds tiago fk.

--- forwardkinematics/urdfFks/tiagoFk.py
+++ forwardkinematics/urdfFks/tiagoFk.py
@@ -7,12 +7,12 @@ class TiagoFk(URDFForwardKinematics):
     def __init__(self, n):
         fileName = "tiago.urdf"
         linkIndices = [1, 2, 3, 4, 5, 7]
-        relevantLinks = ["base_link", "torso_lift_link"]
+        relevantLinks = ["world", "base_link", "torso_lift_link", "torso_lift_link"]
         relevantLinks += ["arm_left_" + str(i) + "_link" for i in linkIndices] + [
             "arm_left_tool_link"
         ]
         relevantLinks += ["arm_right_" + str(i) + "_link" for i in linkIndices] + [
-            "arm_left_tool_link"
+            "arm_right_tool_link"
         ]
         relevantLinks += ["head_1_link", "head_2_link"]
-        super().__init__(fileName, relevantLinks, "base_link", n)
+        super().__init__(fileName, relevantLinks, "world", len(relevantLinks) - 1)

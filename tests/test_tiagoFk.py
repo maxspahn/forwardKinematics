cec02ea1f1845c03da4e2fd991efcd37b06d2962
Max Spahn <m.spahn@tudelft.nl>
2022-03-14 13:37:48 +0100

    Inserts torso joint in forward kinematics.

--- tests/test_tiagoFk.py
+++ tests/test_tiagoFk.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.fixture
 def fk():
-    return TiagoFk(19)
+    return TiagoFk(20)
 
 
 def test_fkZeros(fk):
@@ -22,6 +22,11 @@ def test_fkZeros(fk):
     assert fkNumpy[1] == pytest.approx(0.0, abs=1e-4)
     assert fkNumpy[2] == pytest.approx(0, abs=1e-4)
     fkNumpy = fk.fk(q_np, 2, positionOnly=True)
+    assert isinstance(fkNumpy, np.ndarray)
+    assert fkNumpy[0] == pytest.approx(-0.062, abs=1e-4)
+    assert fkNumpy[1] == pytest.approx(0.0, abs=1e-4)
+    assert fkNumpy[2] == pytest.approx(0.193, abs=1e-4)
+    fkNumpy = fk.fk(q_np, 4, positionOnly=True)
     assert isinstance(fkNumpy, np.ndarray)
     assert fkNumpy[0] == pytest.approx(-0.062, abs=1e-4)
     assert fkNumpy[1] == pytest.approx(0.0, abs=1e-4)
@@ -51,13 +56,18 @@ def test_fkNonZeros(fk):
     assert isinstance(fkNumpy, np.ndarray)
     assert fkNumpy[0] == pytest.approx(x[0], abs=1e-4)
     assert fkNumpy[1] == pytest.approx(x[1], abs=1e-4)
+    assert fkNumpy[2] == pytest.approx(0.193, abs=1e-4)
+    fkNumpy = fk.fk(q_np, 4, positionOnly=True)
+    assert isinstance(fkNumpy, np.ndarray)
+    assert fkNumpy[0] == pytest.approx(x[0], abs=1e-4)
+    assert fkNumpy[1] == pytest.approx(x[1], abs=1e-4)
     assert fkNumpy[2] == pytest.approx(0.193 + 0.597, abs=1e-4)
 
 
 def test_arm(fk):
     q_np = np.zeros(fk.n())
     q_np[5] = -1.0
-    fkNumpy = fk.fk(q_np, 10, positionOnly=True)
+    fkNumpy = fk.fk(q_np, 11, positionOnly=True)
     x = np.array([-0.0544357, 0.676733, 1.15136])
     assert fkNumpy[0] == pytest.approx(x[0], abs=1e-4)
     assert fkNumpy[1] == pytest.approx(x[1], abs=1e-4)
@@ -67,7 +77,7 @@ def test_arm(fk):
 def test_arms_equal(fk):
     """ Tests if both end-effectors have the same z coordinate and opposite y coordinate."""
     q_np = np.zeros(fk.n())
-    fkNumpy_left = fk.fk(q_np, 10, positionOnly=True)
-    fkNumpy_right = fk.fk(q_np, 17, positionOnly=True)
+    fkNumpy_left = fk.fk(q_np, 11, positionOnly=True)
+    fkNumpy_right = fk.fk(q_np, 18, positionOnly=True)
     assert fkNumpy_right[1] == pytest.approx(-fkNumpy_left[1], abs=1e-4)
     assert fkNumpy_right[2] == pytest.approx(fkNumpy_left[2], abs=1e-4)

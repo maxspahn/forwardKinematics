7850ba788610cfe3bf45ef523c3df8d6a5c24c38
Max Spahn <m.spahn@tudelft.nl>
2022-10-20 09:47:11 +0200

    Adds jackal to the list of fk accessible through the FkCreator.

--- tests/test_fkCreator.py
+++ tests/test_fkCreator.py
@@ -1,19 +1,31 @@
-import casadi as ca
 import numpy as np
 from forwardkinematics.fksCommon.fk_creator import FkCreator
 
-robotTypes = ['panda', 'boxer', 'albert', 'tiago', 'planarArm', 'mobilePanda', 'pointRobot', 'groundRobot', 'pointRobotUrdf', 'dualArm']
+robotTypes = [
+    "jackal",
+    "panda",
+    "boxer",
+    "albert",
+    "tiago",
+    "planarArm",
+    "mobilePanda",
+    "pointRobot",
+    "groundRobot",
+    "pointRobotUrdf",
+    "dualArm",
+]
 
 genericRobotTypes = ["planarArm"]
+
 
 def test_fkCreator():
     for robotType in robotTypes:
         print(robotType)
         if robotType in genericRobotTypes:
-            fkCreater = FkCreator(robotType, n=5)
+            fk_creator = FkCreator(robotType, n=5)
         else:
-            fkCreator = FkCreator(robotType)
-        fk = fkCreator.fk()
+            fk_creator = FkCreator(robotType)
+        fk = fk_creator.fk()
         q_np = np.random.random(fk.n())
         fkNumpy = fk.fk(q_np, fk.n(), positionOnly=False)
         assert isinstance(fkNumpy, np.ndarray)

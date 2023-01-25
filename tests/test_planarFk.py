933a1f0b6b23c345977b034ca8c8ff6a4c5dbab9
Max Spahn <m.spahn@tudelft.nl>
2023-01-25 11:02:14 +0100

    Uses pytest.approx instead in test.

--- tests/test_planarFk.py
+++ tests/test_planarFk.py
@@ -1,5 +1,6 @@
 import casadi as ca
 import numpy as np
+import pytest
 from forwardkinematics.planarFks.planarArmFk import PlanarArmFk
 
 
@@ -14,10 +15,8 @@ def test_planarFk():
     assert fkNumpy[2] == 1.0
     fkCasadi_fun = ca.Function('test_fun', [q_ca], [fkCasadi])
     test_eval = np.array(fkCasadi_fun(q_np))[:,0]
-    test_result = np.cos(1.0) + np.cos(1.0)
-    assert test_eval[0] == test_result
-    test_result = np.sin(1.0) + np.sin(1.0)
-    assert test_eval[1] == test_result
+    assert test_eval[0] == pytest.approx(np.cos(1.0) * 1.0 + np.cos(1.0) * 1.0)
+    assert test_eval[1] == pytest.approx(np.sin(1.0) * 1.0 + np.sin(1.0) * 1.0)
     assert test_eval[2] == 1.0
 
 def test_planarFk_by_name():
@@ -31,8 +30,6 @@ def test_planarFk_by_name():
     assert fkNumpy[2] == 1.0
     fkCasadi_fun = ca.Function('test_fun', [q_ca], [fkCasadi])
     test_eval = np.array(fkCasadi_fun(q_np))[:,0]
-    test_result = np.cos(1.0) + np.cos(1.0)
-    assert test_eval[0] == test_result
-    test_result = np.sin(1.0) + np.sin(1.0)
-    assert test_eval[1] == test_result
+    assert test_eval[0] == pytest.approx(np.cos(1.0) * 1.0 + np.cos(1.0) * 1.0)
+    assert test_eval[1] == pytest.approx(np.sin(1.0) * 1.0 + np.sin(1.0) * 1.0)
     assert test_eval[2] == 1.0

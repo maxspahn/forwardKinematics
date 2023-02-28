5f9a9b464fa25384038efd209e3d11ebe385dcbe
Max Spahn <m.spahn@tudelft.nl>
2023-02-28 17:08:43 +0100

    ft[generic]: Adds generic fk for diffdrive.
    Adding generic fk for diff drive robots. If the base_type is specified
    to be `diffdrive`, three additional degrees of freedom are addedd.
    Removes orientation correction joint for boxer and albert.

--- examples/albertFk.py
+++ examples/albertFk.py
@@ -1,21 +1,34 @@
 import casadi as ca
 import numpy as np
 from forwardkinematics.urdfFks.albertFk import AlbertFk
+from forwardkinematics.urdfFks.generic_urdf_fk import GenericURDFFk
 
 
 def main():
-    n = 10
-    q_ca = ca.SX.sym("q", n)
+    q_ca = ca.SX.sym("q", 10)
+    q_ca_2 = ca.SX.sym("q", 11)
+    with open("albert.urdf", "r") as file:
+        urdf = file.read()
     fkPlanar = AlbertFk()
-    q_np = np.random.random(n) * 0.0
+    fk_generic = GenericURDFFk(
+        urdf,
+        rootLink = 'base_link',
+        end_link="panda_ee",
+        base_type='diffdrive',
+    )
+    q_np = np.random.random(10) * 0.0
+    q_np_2 = np.random.random(11) * 0.0
     fkCasadi = fkPlanar.fk(q_ca, 1, positionOnly=True)
     fkNumpy = fkPlanar.fk(q_np, 1, positionOnly=True)
     print(fkNumpy)
     print(fkCasadi)
     fk_casadi_by_name = fkPlanar.casadi_by_name(q_ca, 'panda_rightfinger', positionOnly=True)
     print(fk_casadi_by_name)
-    fk_numpy_by_name = fkPlanar.numpy_by_name(q_np, 'extrusion1', positionOnly=True)
+    fk_numpy_by_name = fkPlanar.numpy_by_name(q_np, 'chassis_link', positionOnly=True)
     print(fk_numpy_by_name)
+
+    fk_casadi_generic = fk_generic.numpy_by_name(q_np_2, 'chassis_link', positionOnly=True)
+    print(fk_casadi_generic)
 
 if __name__ == "__main__":
     main()
--- forwardkinematics/urdfFks/generic_urdf_fk.py
+++ forwardkinematics/urdfFks/generic_urdf_fk.py
@@ -4,13 +4,16 @@ from forwardkinematics.urdfFks.urdfFk import URDFForwardKinematics, LinkNotInURD
 import forwardkinematics.urdfFks.casadiConversion.urdfparser as u2c
 
 class GenericURDFFk(URDFForwardKinematics):
-    def __init__(self, fileName, rootLink='base_link', end_link=None):
+    def __init__(self, fileName, rootLink='base_link', end_link=None, base_type: str = 'holonomic'):
         self._urdf_file = fileName
         self._links = []
         self._end_link = end_link
         self._rootLink = rootLink
         self.readURDF(rootLink, end_link)
         self._n = self.robot.degrees_of_freedom()
+        if base_type in ['diffdrive']:
+            self._q_base = ca.SX.sym("q_base", 3)
+        self._base_type = base_type
         self._q_ca = ca.SX.sym("q", self._n)
         self.generateFunctions()
 
@@ -20,24 +23,42 @@ class GenericURDFFk(URDFForwardKinematics):
         self.robot.detect_link_names()
         self.robot.set_joint_variable_map()
 
+    def generateFunctions(self):
+        self._fks = {}
+        for link in self.robot.link_names():
+            if self._base_type in ['diffdrive']:
+                q = ca.vcat([self._q_base, self._q_ca])
+            else:
+                q = self._q_ca
+            ca_fun = ca.Function(
+                "fk" + link, [q], [self.casadi(q, self._rootLink, link)]
+            )
+            self._fks[link] = ca_fun
+
 
     def casadi(self, q: ca.SX, parent_link: str, child_link: str, positionOnly=False):
-        if parent_link not in self.robot.link_names():
-            raise LinkNotInURDFError(
-                f"""The link you have requested, {parent_link}, is not in the urdf.
-                    Possible links are  {self.robot.link_names()}"""
-            )
         if child_link not in self.robot.link_names():
             raise LinkNotInURDFError(
                 f"""The link you have requested, {child_link}, is not in the urdf.
                     Possible links are  {self.robot.link_names()}"""
             )
-        if positionOnly:
-            return self.robot.get_forward_kinematics(parent_link, child_link, q)["T_fk"][
-                0:3, 3
-            ]
+        if self._base_type in ['diffdrive']:
+            fk = self.robot.get_forward_kinematics(parent_link, child_link, q[2:])["T_fk"]
+            c = ca.cos(q[2])
+            s = ca.sin(q[2])
+            T_base = ca.vcat([
+                ca.hcat([c, -s, 0, q[0]]),
+                ca.hcat([s, c, 0, q[1]]),
+                ca.hcat([0, 0, 1, 0]),
+                ca.hcat([0, 0, 0, 1]),
+            ])
+            fk = ca.mtimes(T_base, fk)
         else:
-            return self.robot.get_forward_kinematics(parent_link, child_link, q)["T_fk"]
+            fk = self.robot.get_forward_kinematics(parent_link, child_link, q)["T_fk"]
+
+        if positionOnly:
+            fk = fk[0:3, 3]
+        return fk
 
     def fk(self, q: ca.SX, parent_link: str, child_link: str, positionOnly=False):
         if isinstance(q, ca.SX):
@@ -46,7 +67,10 @@ class GenericURDFFk(URDFForwardKinematics):
             return self.numpy(q, parent_link, child_link, positionOnly=positionOnly)
 
     def numpy(self, q: ca.SX, parent_link: str, child_link: str, positionOnly=False):
-        fk_parent = super().numpy_by_name(q, parent_link)
+        if parent_link == self._rootLink:
+            fk_parent = np.identity(4)
+        else:
+            fk_parent = super().numpy_by_name(q, parent_link)
         fk_child = super().numpy_by_name(q, child_link)
         tf_parent_child = np.dot(np.linalg.inv(fk_parent), fk_child)
         if positionOnly:

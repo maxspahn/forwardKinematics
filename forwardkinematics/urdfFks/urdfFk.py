662b776b3b2107ecaeec8ff0c9e929ef6ed5a00d
Max Spahn <m.spahn@tudelft.nl>
2024-10-03 21:39:32 +0200

    ft[symbols]: Adds support for passing parameters as symbols to the forward kinematics.

--- forwardkinematics/urdfFks/urdfFk.py
+++ forwardkinematics/urdfFks/urdfFk.py
@@ -1,8 +1,9 @@
-from typing import Union, List
-import numpy as np
-import casadi as ca
-import forwardkinematics.urdfFks.casadiConversion.urdfparser as u2c
+from typing import List, Union
 
+import casadi as ca
+import numpy as np
+
+import forwardkinematics.urdfFks.casadiConversion.urdfparser as u2c
 from forwardkinematics.fksCommon.fk import ForwardKinematics
 
 
@@ -11,7 +12,13 @@ class LinkNotInURDFError(Exception):
 
 
 class URDFForwardKinematics(ForwardKinematics):
-    def __init__(self, urdf: str, root_link: str, end_links: List[str], base_type: str = 'holonomic'):
+    def __init__(
+        self,
+        urdf: str,
+        root_link: str,
+        end_links: List[str],
+        base_type: str = "holonomic",
+    ):
         super().__init__()
         self._urdf = urdf
         self._root_link = root_link
@@ -21,17 +28,19 @@ class URDFForwardKinematics(ForwardKinematics):
         self._n = self.robot.degrees_of_freedom()
         self._q_ca = ca.SX.sym("q", self._n)
         self._mount_transformation = np.identity(4)
-        if base_type in ['diffdrive']:
+        if base_type in ["diffdrive"]:
             self._q_base = ca.SX.sym("q_base", 3)
         self.generate_functions()
 
     def n(self) -> int:
-        if self._base_type == 'diffdrive':
+        if self._base_type == "diffdrive":
             return self._n + 3
         return self._n
 
     def read_urdf(self):
-        self.robot = u2c.URDFparser(root_link=self._root_link, end_links=self._end_links)
+        self.robot = u2c.URDFparser(
+            root_link=self._root_link, end_links=self._end_links
+        )
         self.robot.from_string(self._urdf)
         self.robot.detect_link_names()
         self.robot.set_joint_variable_map()
@@ -39,16 +48,24 @@ class URDFForwardKinematics(ForwardKinematics):
     def generate_functions(self):
         self._fks = {}
         for link in self.robot.link_names():
-            if self._base_type in ['diffdrive']:
+            if self._base_type in ["diffdrive"]:
                 q = ca.vcat([self._q_base, self._q_ca])
             else:
                 q = self._q_ca
-            ca_fun = ca.Function(
-                "fk" + link, [q], [self.casadi(q, link)]
-            )
+            ca_fun = ca.Function("fk" + link, [q], [self.casadi(q, link)])
             self._fks[link] = ca_fun
 
-    def casadi(self, q: ca.SX, child_link: str, parent_link: Union[str, None] = None, link_transformation=np.eye(4), position_only=False):
+    def casadi(
+        self,
+        q: ca.SX,
+        child_link: str,
+        parent_link: Union[str, None] = None,
+        link_transformation=np.eye(4),
+        position_only=False,
+        symbolic_parameters=None,
+    ):
+        if symbolic_parameters is None:
+            symbolic_parameters = {}
         if parent_link is None:
             parent_link = self._root_link
         if child_link not in self.robot.link_names():
@@ -56,26 +73,39 @@ class URDFForwardKinematics(ForwardKinematics):
                 f"""The link you have requested, {child_link}, is not in the urdf.
                     Possible links are  {self.robot.link_names()}"""
             )
-        if self._base_type in ['diffdrive']:
-            fk = self.robot.get_forward_kinematics(parent_link, child_link, q[2:], link_transformation)["T_fk"]
+        if self._base_type in ["diffdrive"]:
+            fk = self.robot.get_forward_kinematics(
+                parent_link, child_link, q[2:], link_transformation
+            )["T_fk"]
             c = ca.cos(q[2])
             s = ca.sin(q[2])
-            T_base = ca.vcat([
-                ca.hcat([c, -s, 0, q[0]]),
-                ca.hcat([s, c, 0, q[1]]),
-                ca.hcat([0, 0, 1, 0]),
-                ca.hcat([0, 0, 0, 1]),
-            ])
+            T_base = ca.vcat(
+                [
+                    ca.hcat([c, -s, 0, q[0]]),
+                    ca.hcat([s, c, 0, q[1]]),
+                    ca.hcat([0, 0, 1, 0]),
+                    ca.hcat([0, 0, 0, 1]),
+                ]
+            )
             fk = ca.mtimes(T_base, fk)
         else:
-            fk = self.robot.get_forward_kinematics(parent_link, child_link, q, link_transformation)["T_fk"]
+            fk = self.robot.get_forward_kinematics(
+                parent_link, child_link, q, link_transformation, symbolic_parameters
+            )["T_fk"]
             fk = ca.mtimes(self._mount_transformation, fk)
 
         if position_only:
             fk = fk[0:3, 3]
         return fk
 
-    def numpy(self, q: ca.SX, child_link: str, parent_link: Union[str, None] = None, link_transformation=np.eye(4), position_only=False):
+    def numpy(
+        self,
+        q: ca.SX,
+        child_link: str,
+        parent_link: Union[str, None] = None,
+        link_transformation=np.eye(4),
+        position_only=False,
+    ):
         if child_link not in self._fks and child_link != self._root_link:
             raise LinkNotInURDFError(
                 f"""The link you have requested, {child_link}, is not in the urdf.
@@ -97,9 +127,10 @@ class URDFForwardKinematics(ForwardKinematics):
         else:
             fk_child = self._fks[child_link](q)
         tf_parent_child = np.dot(np.linalg.inv(fk_parent), fk_child)
-        tf_parent_child = np.dot(link_transformation, tf_parent_child) #ToDo check if correct
+        tf_parent_child = np.dot(
+            link_transformation, tf_parent_child
+        )  # ToDo check if correct
         if position_only:
             return tf_parent_child[0:3, 3]
         else:
             return tf_parent_child
-
--- tests/test_pandaFk.py
+++ tests/test_pandaFk.py
@@ -1,11 +1,12 @@
 import os
+
 import casadi as ca
 import numpy as np
 import pytest
 
-from forwardkinematics import GenericURDFFk
-from forwardkinematics import GenericXMLFk
+from forwardkinematics import GenericURDFFk, GenericXMLFk
 from forwardkinematics.urdfFks.urdfFk import LinkNotInURDFError
+
 
 @pytest.fixture
 def fk() -> GenericURDFFk:
@@ -14,10 +15,11 @@ def fk() -> GenericURDFFk:
         urdf = file.read()
     fk_panda = GenericURDFFk(
         urdf,
-        root_link = 'panda_link0',
+        root_link="panda_link0",
         end_links="panda_leftfinger",
     )
     return fk_panda
+
 
 @pytest.fixture
 def fk_xml() -> GenericXMLFk:
@@ -26,23 +28,26 @@ def fk_xml() -> GenericXMLFk:
         xml = file.read()
     fk_panda = GenericXMLFk(
         xml,
-        root_link = 'panda_link0',
+        root_link="panda_link0",
         end_links="panda_leftfinger",
     )
     return fk_panda
 
+
 def test_pandaFk(fk):
     q_ca = ca.SX.sym("q", 7)
     q_np = np.random.random(7)
-    fkCasadi = fk.casadi(q_ca, 'panda_link9', position_only=False)
-    fkNumpy = fk.numpy(q_np, 'panda_link9', position_only=False)
+    fkCasadi = fk.casadi(q_ca, "panda_link9", position_only=False)
+    fkNumpy = fk.numpy(q_np, "panda_link9", position_only=False)
     assert isinstance(fkCasadi, ca.SX)
     assert isinstance(fkNumpy, np.ndarray)
 
+
 def test_xmlFk(fk_xml):
     q_ca = ca.SX.sym("q", 9)
-    fk_casadi = fk_xml.casadi(q_ca, 'link7', position_only=False)
+    fk_casadi = fk_xml.casadi(q_ca, "link7", position_only=False)
     assert isinstance(fk_casadi, ca.SX)
+
 
 def test_compare_xml_urdf(fk, fk_xml):
     """
@@ -52,44 +57,76 @@ def test_compare_xml_urdf(fk, fk_xml):
     """
     q_ca = ca.SX.sym("q", 9)
     for i in range(8):
-        fk_casadi_urdf = fk.casadi(q_ca, f'panda_link{i}', position_only=False)
-        fk_casadi_xml = fk_xml.casadi(q_ca, f'link{i}_c', position_only=False)
+        fk_casadi_urdf = fk.casadi(q_ca, f"panda_link{i}", position_only=False)
+        fk_casadi_xml = fk_xml.casadi(q_ca, f"link{i}_c", position_only=False)
         print(fk_casadi_xml)
         assert fk_casadi_urdf.shape == fk_casadi_xml.shape
-        fk_xml_function = ca.Function('fk_xml', [q_ca], [fk_casadi_xml])
-        fk_urdf_function = ca.Function('fk_urdf', [q_ca], [fk_casadi_urdf])
+        fk_xml_function = ca.Function("fk_xml", [q_ca], [fk_casadi_xml])
+        fk_urdf_function = ca.Function("fk_urdf", [q_ca], [fk_casadi_urdf])
         q_np = np.random.random(9)
         fk_np_xml = fk_xml_function(q_np)
         fk_np_urdf = fk_urdf_function(q_np)
         assert np.allclose(fk_np_xml, fk_np_urdf, atol=1e-4)
-        fk_np_direct_xml = fk_xml.numpy(q_np, f'link{i}_c', position_only=False)
+        fk_np_direct_xml = fk_xml.numpy(q_np, f"link{i}_c", position_only=False)
         assert np.allclose(fk_np_xml, fk_np_direct_xml, atol=1e-4)
 
+
 def test_pandaFkByName(fk):
-    q_ca = ca.SX.sym('q', 7)
+    q_ca = ca.SX.sym("q", 7)
     q_np = np.random.random(7)
-    fkCasadi = fk.casadi(q_ca, 'panda_link3', position_only=False)
+    fkCasadi = fk.casadi(q_ca, "panda_link3", position_only=False)
     assert isinstance(fkCasadi, ca.SX)
 
+
 def test_pandafkByWrongName(fk):
-    q_ca = ca.SX.sym('q', 7)
+    q_ca = ca.SX.sym("q", 7)
     with pytest.raises(LinkNotInURDFError):
-        fkCasadi = fk.casadi(q_ca, 'panda_link10', position_only=False)
+        fkCasadi = fk.casadi(q_ca, "panda_link10", position_only=False)
+
 
 def test_simpleFk(fk: GenericURDFFk):
     q_np = np.array([0.0000, 1.0323, 0.0000, 0.8247, 0.0000, 0.2076, 0.0000])
-    fkNumpy = fk.numpy(q_np, 'panda_link0', position_only=True)
+    fkNumpy = fk.numpy(q_np, "panda_link0", position_only=True)
     x_ee = np.array([0.0, 0.0, 0.0])
     assert fkNumpy[0] == pytest.approx(x_ee[0], abs=1e-4)
     assert fkNumpy[1] == pytest.approx(x_ee[1], abs=1e-4)
     assert fkNumpy[2] == pytest.approx(x_ee[2], abs=1e-4)
-    fkNumpy = fk.numpy(q_np, 'panda_link3', position_only=True)
+    fkNumpy = fk.numpy(q_np, "panda_link3", position_only=True)
     x_ee = np.array([0.2713, 0.0, 0.4950])
     assert fkNumpy[0] == pytest.approx(x_ee[0], abs=1e-4)
     assert fkNumpy[1] == pytest.approx(x_ee[1], abs=1e-4)
     assert fkNumpy[2] == pytest.approx(x_ee[2], abs=1e-4)
-    fkNumpy = fk.numpy(q_np, 'panda_link9', position_only=True)
+    fkNumpy = fk.numpy(q_np, "panda_link9", position_only=True)
     x_ee = np.array([0.4, 0.0, 0.71])
     assert fkNumpy[0] == pytest.approx(x_ee[0], abs=1e-4)
     assert fkNumpy[1] == pytest.approx(x_ee[1], abs=1e-4)
     assert fkNumpy[2] == pytest.approx(x_ee[2], abs=1e-4)
+
+
+def test_symbolic_fk(fk: GenericURDFFk):
+    q_ca = ca.SX.sym("q", 7)
+    panda_joint1_z = ca.SX.sym("panda_joint1_z")
+    panda_joint3_roll = ca.SX.sym("panda_joint3_roll")
+    symbolic_parameters = {
+        "panda_joint1": {
+            "z": panda_joint1_z,
+        },
+        "panda_joint3": {"roll": panda_joint3_roll},
+    }
+
+    link_name = "panda_link5"
+    fkCasadi = fk.casadi(
+        q_ca, link_name, position_only=False, symbolic_parameters=symbolic_parameters
+    )
+    assert isinstance(fkCasadi, ca.SX)
+    fk_casadi_function = ca.Function(
+        "fk", [q_ca, panda_joint1_z, panda_joint3_roll], [fkCasadi]
+    )
+    q_np = np.random.random(7)
+    z = 0.333
+    roll = 1.57079632679
+    fk_np = fk.numpy(q_np, link_name, position_only=False)
+    fk_symbolic_np = fk_casadi_function(q_np, z, roll)
+    print(fk_np)
+    print(fk_symbolic_np)
+    assert np.allclose(fk_np, fk_symbolic_np, atol=1e-4)
